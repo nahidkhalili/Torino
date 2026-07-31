331f4a38f273e4b169b18e892e7d1ba67fac2f31
Nahid Khalili <Nahid_khalili@ymail.com>
2026-07-31 21:53:02 +0330

    added yup and react hook form in checkotp and sendotp components

--- components/molecules/CheckOTPForm.js
+++ components/molecules/CheckOTPForm.js
@@ -8,63 +8,89 @@ import { useCheckOtp } from "../../services/mutations";
 import { setCookie } from "../../utils/cookie";
 import { useModalContext } from "../../providers/contextProvider";
 
+import * as yup from "yup";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object({
+  code: yup
+    .string()
+    .length(6, "طول کد باید 6 رقم باشد")
+    .matches(/^[0-9]+$/, "کد باید عدد باشد")
+    .required("کد را وارد کنید"),
+});
+
 function CheckOTPForm({ mobile, setStep, setIsOpen }) {
-  const [code, setCode] = useState("");
+  const {
+    control,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
 
   const { isPending, mutate } = useCheckOtp();
   const { setUser, handleLogin } = useModalContext();
 
-  const checkOtpHandler = (event) => {
-    event.preventDefault();
-
+  const checkOtpHandler = ({ code }) => {
     if (isPending) return;
+    console.log("code:", code);
 
     mutate(
       { mobile, code },
       {
-        onSuccess: (data) => {
-          setCookie("accessToken", data?.data?.accessToken, 30);
-          setCookie("refreshToken", data?.data?.refreshToken, 365);
+        onSuccess: (response) => {
+          setCookie("accessToken", response?.data?.accessToken, 30);
+          setCookie("refreshToken", response?.data?.refreshToken, 365);
           setIsOpen(false);
           // setUser(mobile);
           handleLogin(mobile);
           setStep(1);
         },
         onError: (error) => {
-          console.log(error);
+          console.log("an error occured", error.message);
+          setError("code", { type: "server", message: error.message });
         },
-      }
+      },
     );
-  };
-
-  const changeHandler = (otp) => {
-    setCode(otp);
   };
 
   return (
     <div className={styles.container}>
       <h4 className={styles.title}>کد تایید را وارد کنید.</h4>
-      <form className={styles.form} onSubmit={checkOtpHandler}>
+      <form className={styles.form} onSubmit={handleSubmit(checkOtpHandler)}>
         <label>کد تایید به شماره موبایل {mobile} ارسال شد.</label>
         <div className={styles.otpContainer}>
-          <OtpInput
-            value={code}
-            onChange={changeHandler}
-            numInputs={6}
-            shouldAutoFocus={true}
-            inputStyle={{
-              border: "1px solid silver",
-              borderRadius: "5px",
-              width: "58px",
-              height: "53px",
-              height: "45px",
-              marginRight: "5px",
-            }}
+          <Controller
+            name="code"
+            control={control}
+            render={({ field }) => (
+              <OtpInput
+                value={field.value || ""}
+                onChange={field.onChange}
+                onBlur={field.onBlur}
+                ref={field.ref}
+                numInputs={6}
+                shouldAutoFocus={true}
+                inputStyle={{
+                  border: "1px solid silver",
+                  borderRadius: "5px",
+                  width: "58px",
+                  height: "53px",
+                  height: "45px",
+                  marginRight: "5px",
+                }}
+              />
+            )}
           />
+
+          {errors.code && (
+            <p className={styles.serverError}>{errors.code.message}</p>
+          )}
         </div>
         <button className={styles.button} type="submit">
           ورود به تورینو
         </button>
+        {/* {errors.code && console.log("hello:", errors.code.message)} */}
       </form>
     </div>
   );
--- components/molecules/SendOTPForm.js
+++ components/molecules/SendOTPForm.js
@@ -1,56 +1,102 @@
 "use client";
 
-import { useState } from "react";
+// import { useState } from "react";
 import toast from "react-hot-toast";
 
 import styles from "./OTP.module.css";
 
 import { useSendOtp } from "../../services/mutations";
-import { isValidMobile } from "../../utils/validation";
+// import { isValidMobile } from "../../utils/validation";
 
-function SendOTPForm({ mobile, setMobile, setStep, setExpired }) {
-  const [error, setError] = useState("");
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object({
+  mobile: yup
+    .string()
+    .trim()
+    .required("شماره موبایل خود را وارد کنید")
+    .matches(/^09\d{9}$/, "شماره موبایل معتبر نیست"),
+});
+
+function SendOTPForm({ setMobile, setStep, setExpired }) {
+  // const [error, setError] = useState("");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ resolver: yupResolver(schema) });
 
   const { isPending, mutate } = useSendOtp();
 
-  const sendOtpHandler = (event) => {
-    event.preventDefault();
-
+  const sendOtpHandler = ({ mobile }) => {
     if (isPending) return;
-    if (!isValidMobile(mobile)) return setError("شماره معتبر وارد کنید!");
-    setError("");
-
     mutate(
       { mobile },
       {
-        onSuccess: (data) => {
-          toast.success(data?.data?.message);
-          toast(data?.data?.code);
-          console.log("hi", data?.data.code);
+        onSuccess: (response) => {
+          setMobile(mobile);
+          toast.success(response?.data?.message);
+          toast(response?.data.code);
+          console.log("sendOtpResponse:", response);
           setStep(2);
         },
         onError: (error) => {
-          console.log(error);
+          console.log("sendOtpError:", error);
         },
-      }
+      },
     );
   };
+
+  // const sendOtpHandler = (event) => {
+  //   event.preventDefault();
+
+  //   if (isPending) return;
+  //   if (!isValidMobile(mobile)) return setError("شماره معتبر وارد کنید!");
+  //   setError("");
+
+  //   mutate(
+  //     { mobile },
+  //     {
+  //       onSuccess: (data) => {
+  //         toast.success(data?.data?.message);
+  //         toast(data?.data?.code);
+  //         console.log("hi", data);
+  //         setStep(2);
+  //       },
+  //       onError: (error) => {
+  //         console.log(error);
+  //       },
+  //     }
+  //   );
+  // };
 
   return (
     <div className={styles.container}>
       <h4 className={styles.title}>ورود به تورینو</h4>
-      <form className={styles.form} onSubmit={sendOtpHandler}>
-        <label>شماره موبایل خود را وارد کنید</label>
-        <input
+      <form className={styles.form} onSubmit={handleSubmit(sendOtpHandler)}>
+        <label htmlFor="mobile">شماره موبایل خود را وارد کنید</label>
+        {/* <input
           className={styles.input}
           type="text"
           placeholder="۳۳۱۳****۰۹۱۲"
           value={mobile}
           onChange={(e) => setMobile(e.target.value)}
+        /> */}
+
+        <input
+          type="tel"
+          className={styles.input}
+          placeholder="۳۳۱۳****۰۹۱۲"
+          id="mobile"
+          {...register("mobile")}
         />
-        {!!error && <p className={styles.error}>{error}</p>}
-        <button className={styles.button} type="submit">
-          ارسال کد تایید
+        {errors.mobile && (
+          <p className={styles.error}>{errors.mobile.message}</p>
+        )}
+        <button disabled={isPending} className={styles.button} type="submit">
+          {isPending ? " در حال دریافت کد " : " ارسال کد تایید"}
         </button>
       </form>
     </div>
